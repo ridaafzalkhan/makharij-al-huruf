8e0ef4a6db4cd460b734686280b41de7c7817c25
ridaafzalkhan <ridaafzal0@gmail.com>
2021-11-09 03:07:21 +0500

    second commit

--- app/src/main/java/rida/org/makharijal_huruf/MainActivity2.java
+++ app/src/main/java/rida/org/makharijal_huruf/MainActivity2.java
@@ -2,13 +2,87 @@ package rida.org.makharijal_huruf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button Halqiyah ,Lahatiyah,Shajariyah,Tarfiyah,Niteeyah,Lisaveyah,Ghunna;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
+        Halqiyah = (Button) findViewById(R.id.Halqiyah);
+        Halqiyah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity2.this, MainActivity3.class);
+                newIntent.putExtra("buttonName", "Halqiyah");
+                startActivity(newIntent);
+            }
+        });
+        Lahatiyah = (Button) findViewById(R.id.Lahatiyah);
+        Lahatiyah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity2.this, MainActivity3.class);
+                newIntent.putExtra("buttonName", "Lahatiyah");
+                startActivity(newIntent);
+
+            }
+        });
+        Shajariyah = (Button) findViewById(R.id.Shajariyah);
+        Shajariyah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity2.this, MainActivity3.class);
+                newIntent.putExtra("buttonName", "Shajariyah");
+                startActivity(newIntent);
+
+            }
+        });
+        Tarfiyah = (Button) findViewById(R.id.Tarfiyah);
+        Tarfiyah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity2.this, MainActivity3.class);
+                newIntent.putExtra("buttonName", "Tarfiyah");
+                startActivity(newIntent);
+
+            }
+        });
+        Niteeyah = (Button) findViewById(R.id.Niteeyah);
+        Niteeyah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity2.this, MainActivity3.class);
+                newIntent.putExtra("buttonName", "Niteeyah");
+                startActivity(newIntent);
+
+            }
+        });
+        Lisaveyah = (Button) findViewById(R.id.Lisaveyah);
+        Lisaveyah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity2.this, MainActivity3.class);
+                newIntent.putExtra("buttonName", "Lisaveyah");
+                startActivity(newIntent);
+
+            }
+        });
+        Ghunna = (Button) findViewById(R.id.Ghunna);
+        Ghunna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity2.this, MainActivity3.class);
+                newIntent.putExtra("buttonName", "Ghunna");
+                startActivity(newIntent);
+
+            }
+        });
     }
 }

eed18f802bccc8e0e4215c5e1bc072681d25edaf
ridaafzalkhan <ridaafzal0@gmail.com>
2021-11-11 09:14:55 +0500

    final commit

--- app/src/main/java/rida/org/makharijal_huruf/MainActivity.java
+++ app/src/main/java/rida/org/makharijal_huruf/MainActivity.java
@@ -8,10 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     Button btn1, btn2, btn3;
-
+    ImageView img1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         btn1 = (Button) findViewById(R.id.button);
         btn2= (Button) findViewById(R.id.test);
         btn3= (Button) findViewById(R.id.repo);
+        img1 = (ImageView) findViewById(R.id.imageView);
+        img1.setBackgroundResource(R.drawable.git);
         if(btn1!=null) {
             btn1.setOnClickListener(new View.OnClickListener() {
                 @Override
